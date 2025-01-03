39c02ad01dbaeba64cc8ae2943b2855a6a26c5f0
James Hatfield <jahatfi@gmail.com>
2025-01-03 01:26:56 -0500

    Better example in README.md

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 This is a semantic tool to map CISA NICE cybersecurity tasks, knowledge, and skils, to the corresponding ones (aka KSAT) as implemented in the DoD 8140 framework.
+
+e.g.
+
+```
+python semantic_search.py
+10      Knowledge of application vulnerabilities
+10A     Skill in conducting vulnerability scans and recognizing vulnerabilities in information systems and networks
+1157    Ensure that AI design and development activities are properly documented and updated.
+142A    Knowledge of the operations and processes for incident, problem, and event management.
+Embedding NICE tasks...
+
+8140 KSAT #10: Knowledge of application vulnerabilities
+Top 5 most similar CISA NICE tasks:
+NICE ID: T1118 (Score: 0.7025) "Identify vulnerabilities"
+NICE ID: T0686 (Score: 0.6576) "Identify threat vulnerabilities"
+NICE ID: T1690 (Score: 0.6433) "Identify exploitable technical or operational vulnerabilities"
+NICE ID: T1028 (Score: 0.6283) "Research new vulnerabilities in emerging technologies"
+NICE ID: T1559 (Score: 0.6146) "Resolve vulnerabilities in systems and system components"
+
+8140 KSAT #10A: Skill in conducting vulnerability scans and recognizing vulnerabilities in information systems and networks
+Top 5 most similar CISA NICE tasks:
+NICE ID: T1118 (Score: 0.6574) "Identify vulnerabilities"
+NICE ID: T0686 (Score: 0.6142) "Identify threat vulnerabilities"
+NICE ID: T1619 (Score: 0.6113) "Perform risk and vulnerability assessments"
+NICE ID: T1690 (Score: 0.5987) "Identify exploitable technical or operational vulnerabilities"
+NICE ID: T1747 (Score: 0.5938) "Identify system vulnerabilities within a network"
+
+8140 KSAT #1157: Ensure that AI design and development activities are properly documented and updated.
+Top 5 most similar CISA NICE tasks:
+NICE ID: T1704 (Score: 0.4816) "Develop intelligence operations plans"
+NICE ID: T1750 (Score: 0.4789) "Identify intelligence environment preparation derived production needs"
+NICE ID: T1115 (Score: 0.4550) "Oversee the development of design solutions"
+NICE ID: T1798 (Score: 0.4516) "Provide intelligence analysis and support"
+NICE ID: T1835 (Score: 0.4369) "Determine if intelligence requirements and collection plans are accurate and up-to-date"
+
+8140 KSAT #142A: Knowledge of the operations and processes for incident, problem, and event management.
+Top 5 most similar CISA NICE tasks:
+NICE ID: T1427 (Score: 0.5890) "Maintain incident tracking and solution databases"
+NICE ID: T1977 (Score: 0.5525) "Coordinate with internal and external incident management partners across jurisdictions"
+NICE ID: T1333 (Score: 0.5502) "Communicate incident findings to appropriate constituencies"
+NICE ID: T1986 (Score: 0.5489) "Develop a continuously updated overview of an incident throughout the incident's life cycle"
+NICE ID: T1538 (Score: 0.5427) "Resolve customer-reported system incidents and events"
+That took 9.631466150283813 s
+```
--- semantic_search.py
+++ semantic_search.py
@@ -9,51 +9,55 @@ This script outputs for various queries the top 5 most similar sentences in the 
 import time
 start = time.time()
 
+
+
+NICE_corpus = []
+NICE_text_to_ids = {}
+
+with open("NICE_tasks.txt", encoding="utf-8") as fp:
+    for line in fp:
+        line = line.strip()
+
+        id, text = line.split('\t')
+        NICE_corpus.append(text)
+        NICE_text_to_ids[text] = id
+
+# Query sentences:
+DOD_corpus = []
+DOD_text_to_ids = {}
+with open("DOD_8140_ksats.txt", encoding="utf-8") as fp:
+    for line in fp:
+        line = line.strip()
+        print(line)
+        id, text = line.split('\t')
+        DOD_corpus.append(text)
+        DOD_text_to_ids[text] = id
+
+# Use "convert_to_tensor=True" to keep the tensors on GPU (if available)
 import torch
 
 from sentence_transformers import SentenceTransformer
 
 embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
-# Corpus with example sentences
-corpus = []
-nice = {}
+print("Embedding NICE tasks...")
+corpus_embeddings = embedder.encode(NICE_corpus, convert_to_tensor=True)
 
-with open("NICE_tks.txt", encoding="utf-8") as fp:
-    for line in fp:
-        line = line.strip()
-
-        id, text = line.split('\t')
-        corpus.append(text)
-        nice[text] = id
-
-
-# Use "convert_to_tensor=True" to keep the tensors on GPU (if available)
-print("Embedding corpus...")
-corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
-print("Executing query")
-# Query sentences:
-queries = [
-    "Skill in conducting vulnerability scans and recognizing vulnerabilities in information systems and networks",
-    "Knowledge of application vulnerabilities.",    	
-    "Ensure that AI design and development activities are properly documented and updated.",
-    "Knowledge of the operations and processes for incident, problem, and event management.",
-]
 
 # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
-top_k = min(5, len(corpus))
-for query in queries:
-    query_embedding = embedder.encode(query, convert_to_tensor=True)
+top_k = min(5, len(NICE_corpus))
+for ksat in DOD_corpus:
+    query_embedding = embedder.encode(ksat, convert_to_tensor=True)
 
     # We use cosine-similarity and torch.topk to find the highest 5 scores
     similarity_scores = embedder.similarity(query_embedding, corpus_embeddings)[0]
     scores, indices = torch.topk(similarity_scores, k=top_k)
 
-    print("\n8140 Query:", query)
-    print("Top 5 most similar sentences in corpus:")
+    print(f"\n8140 KSAT #{DOD_text_to_ids[ksat]}: {ksat}")
+    print("Top 5 most similar CISA NICE tasks:")
 
     for score, idx in zip(scores, indices):
-        print(f"NICE ID: {nice[corpus[idx]]} (Score: {score:.4f}) \"{corpus[idx]}\"")
+        print(f"NICE ID: {NICE_text_to_ids[NICE_corpus[idx]]} (Score: {score:.4f}) \"{NICE_corpus[idx]}\"")
 
     """
     # Alternatively, we can also use util.semantic_search to perform cosine similarty + topk
